7413736193d63444a63474479d15fd9aae0ffd10
HakanDARAK <hakandarak1@gmail.com>
2021-12-10 02:34:03 +0300

    Proje dosyası ekle.

---
+++ MCD_FormGiriş/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace MCD_FormGiriş
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnToplama = new System.Windows.Forms.Button();
+            this.btnCıkarma = new System.Windows.Forms.Button();
+            this.btnCarpma = new System.Windows.Forms.Button();
+            this.btnBolme = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(0, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Sayı";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.AccessibleName = "number1";
+            this.txtNumber1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNumber1.Location = new System.Drawing.Point(45, 35);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(100, 21);
+            this.txtNumber1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(0, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "2. Sayi";
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.AccessibleName = "number2";
+            this.txtNumber2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtNumber2.Location = new System.Drawing.Point(45, 63);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(100, 21);
+            this.txtNumber2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(42, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Basit Hesap Makinesi";
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.AccessibleName = "result";
+            this.txtSonuc.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSonuc.Location = new System.Drawing.Point(45, 90);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(100, 21);
+            this.txtSonuc.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(3, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Sonuç";
+            // 
+            // btnToplama
+            // 
+            this.btnToplama.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnToplama.Location = new System.Drawing.Point(151, 25);
+            this.btnToplama.Name = "btnToplama";
+            this.btnToplama.Size = new System.Drawing.Size(75, 23);
+            this.btnToplama.TabIndex = 6;
+            this.btnToplama.Text = "Toplama";
+            this.btnToplama.UseVisualStyleBackColor = true;
+            this.btnToplama.Click += new System.EventHandler(this.btnToplama_Click);
+            // 
+            // btnCıkarma
+            // 
+            this.btnCıkarma.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCıkarma.Location = new System.Drawing.Point(151, 52);
+            this.btnCıkarma.Name = "btnCıkarma";
+            this.btnCıkarma.Size = new System.Drawing.Size(75, 23);
+            this.btnCıkarma.TabIndex = 7;
+            this.btnCıkarma.Text = "Çıkarma";
+            this.btnCıkarma.UseVisualStyleBackColor = true;
+            this.btnCıkarma.Click += new System.EventHandler(this.btnCıkarma_Click);
+            // 
+            // btnCarpma
+            // 
+            this.btnCarpma.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCarpma.Location = new System.Drawing.Point(151, 81);
+            this.btnCarpma.Name = "btnCarpma";
+            this.btnCarpma.Size = new System.Drawing.Size(75, 23);
+            this.btnCarpma.TabIndex = 8;
+            this.btnCarpma.Text = "Çarpma";
+            this.btnCarpma.UseVisualStyleBackColor = true;
+            this.btnCarpma.Click += new System.EventHandler(this.btnCarpma_Click);
+            // 
+            // btnBolme
+            // 
+            this.btnBolme.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBolme.Location = new System.Drawing.Point(151, 110);
+            this.btnBolme.Name = "btnBolme";
+            this.btnBolme.Size = new System.Drawing.Size(75, 23);
+            this.btnBolme.TabIndex = 9;
+            this.btnBolme.Text = "Bölme";
+            this.btnBolme.UseVisualStyleBackColor = true;
+            this.btnBolme.Click += new System.EventHandler(this.btnBolme_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(238, 160);
+            this.Controls.Add(this.btnBolme);
+            this.Controls.Add(this.btnCarpma);
+            this.Controls.Add(this.btnCıkarma);
+            this.Controls.Add(this.btnToplama);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSonuc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSonuc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnToplama;
+        private System.Windows.Forms.Button btnCıkarma;
+        private System.Windows.Forms.Button btnCarpma;
+        private System.Windows.Forms.Button btnBolme;
+    }
+}
+
---
+++ MCD_FormGiriş/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCD_FormGiriş
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToplama_Click(object sender, EventArgs e)
+        {
+            double number1, number2, sonuc = 0;
+            number1 = Convert.ToDouble(txtNumber1.Text);
+            number2 = Convert.ToDouble(txtNumber2.Text);
+            sonuc = number1 + number2;
+            txtSonuc.Text = sonuc.ToString();
+        }
+
+        private void btnCıkarma_Click(object sender, EventArgs e)
+        {
+            double number1, number2, sonuc = 0;
+            number1 = Convert.ToDouble(txtNumber1.Text);
+            number2 = Convert.ToDouble(txtNumber2.Text);
+            if (number2 > number1)
+            {
+                DialogResult decision = new DialogResult();
+               decision= MessageBox.Show("1. girdiğiniz sayı 2. sayıdan büyük 2. sayıdan birinci sayıyı çıkaralım mı", "Düzeltme", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                
+
+                if (decision == DialogResult.Yes)
+                {
+                    sonuc = number1 - number2;
+                    txtSonuc.Text = Math.Abs(sonuc).ToString();
+                }
+               
+            }
+            else
+            {
+                sonuc = number1 - number2;
+                txtSonuc.Text = sonuc.ToString();
+            }
+
+
+
+        }
+
+        private void btnCarpma_Click(object sender, EventArgs e)
+        {
+            double number1, number2, sonuc = 0;
+            number1 = Convert.ToDouble(txtNumber1.Text);
+            number2 = Convert.ToDouble(txtNumber2.Text);
+            sonuc = number1 * number2;
+            txtSonuc.Text = sonuc.ToString();
+        }
+
+        private void btnBolme_Click(object sender, EventArgs e)
+        {
+            double number1, number2, sonuc = 0;
+            number1 = Convert.ToDouble(txtNumber1.Text);
+            number2 = Convert.ToDouble(txtNumber2.Text);
+            if (number2 == 0)
+            {
+                MessageBox.Show("HATA! Tanımsız ifade görünen o ki sıfıra bölmeye çalışmıssın düzelt!", "Tanımsız İfade", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                sonuc = number1 * number2;
+                txtSonuc.Text = sonuc.ToString();
+            }
+
+
+        }
+    }
+}
